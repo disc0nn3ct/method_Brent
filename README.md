02d807caaf873d7742176e2b2a459b7191e2d0b5
c0nn3ct <fsa1999fsa@yandex.ru>
2021-05-17 00:43:31 +0300

    NTL Add

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # method_Brent
 Метод Брента 
+
+
+
+Используется библиотека [NTL](https://github.com/libntl/ntl)
+
+Установка: 
+```shell
+
+   % gunzip ntl-xxx.tar.gz
+   % tar xf ntl-xxx.tar
+   % cd ntl-xxx/src
+   % ./configure 
+   % make
+   % make check
+   % sudo make install
+
+```
+
+
+Компиляция:
+```shell
+g++ -g -O2 -std=c++11 -pthread -march=native one.cpp -lntl -lgmp -lm
+```
+
--- one.cpp
+++ one.cpp
@@ -2,18 +2,40 @@
 #include <algorithm> 
 
 
+
+#include <NTL/ZZ.h>
+
 #include <cmath>
 
-typedef long long ll;
+
 
 // (x+1)*(x+1)
-inline ll f(ll x) { return (x*x+1); }
+inline NTL::ZZ f(NTL::ZZ x) { return (x*x+1); }
+
+// template <class T> T f(T x) { return x*x+1; }
+
+
+template <class T, class T_power> 
+T super_pow(T a, T_power c)
+{
+
+
+}
+
+template <class T> 
+T method_Pollard(T n, T number_of)
+{
+
+} 
 
 
 
-ll method_Pollard(ll n, ll seed = 1) {
-    ll x = seed, y = seed;
-    ll divisor = 1;
+
+
+
+NTL::ZZ method_Pollard(NTL::ZZ n, NTL::ZZ seed = 1) {
+    NTL::ZZ x = seed, y = seed;
+    NTL::ZZ divisor = 1;
     while (divisor == 1 || divisor == n) {
         y = f(y) % n;
         x = f(f(x) % n) % n;
@@ -26,19 +48,19 @@ ll method_Pollard(ll n, ll seed = 1) {
 
 
 
-ll method_Brent(ll m, uint c)
+NTL::ZZ method_Brent(NTL::ZZ m, uint c)
 {
     std::cout << "Будет произведено " << " c = " << c << ", " << pow(2, c) << " шагов " << std::endl;    
     if (c == 0)
         return 0;
-    ll z=0,x=rand() % m;
+    NTL::ZZ z=0,x=rand() % m;
     int n=0, k=0, t=1;
     to_3:
     std::cout  << "x = " << x << " t = " << t << " n = " << n << " k = " << k <<" z = " << z << std::endl;    
     x = f(x) % m;
     n++;
 ////
-    ll p;
+    NTL::ZZ p;
     p= std::__gcd(m, std::abs(z-x));
     if( m>p && p>1)
     {
@@ -54,7 +76,7 @@ ll method_Brent(ll m, uint c)
         t=t*2;
     }
     if (k > c)
-        return -1;
+        return 1;
     else 
         goto to_3;
     // ll p;
@@ -66,17 +88,20 @@ ll method_Brent(ll m, uint c)
     //     goto to_3;
 }
 
-
 int main()
 {
-    long long k;
+    NTL::ZZ k;
     // std::cin >> k;
     // k = 4294967297;
     // k = 112;
     k =4;
     // std::cout << method_Pollard(k) << std::endl; // 4294967297
     std::cout << "======" << std::endl;
-    std::cout << method_Brent(k, 5) << std::endl; // 4294967297
+    // std::cout << method_Brent(k, 5) << std::endl; // 4294967297
 
+    NTL::ZZ t, r, l;
+	t=2;
+	r=-1; l=3;
+    std::cout << "PER " << abs(t-l) << "  === " << r << std::endl;
     return 0;
 }
